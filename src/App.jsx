e62e9a725ab05b18d681e950a66abeffd8b0b620
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2024-09-17 15:21:00 +0200

    cssbaseline

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Calendar from "./Calendar";
+import CssBaseline from "@mui/material/CssBaseline";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -8,6 +9,7 @@ import "@fontsource/roboto/700.css";
 function App() {
   return (
     <>
+      <CssBaseline />
       <Calendar />
     </>
   );
