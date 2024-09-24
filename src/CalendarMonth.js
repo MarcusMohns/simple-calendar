92bccaed880699f02fa1fe9655baef40c6e6992b
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2024-09-24 14:45:49 +0200

    add first dates of next month

---
+++ src/CalendarMonth.js
@@ -0,0 +1,72 @@
+import { MONTHS } from "./Utilities";
+import { DAYS } from "./Utilities";
+
+export const CalendarMonth = (year, month) => {
+  const date = new Date(year, month);
+
+  let dates = [];
+  const monthInt = parseInt(month);
+  const yearInt = parseInt(year);
+
+  // Get the last date of the month
+  let lastDate = new Date(yearInt, monthInt + 1, 0).getDate();
+
+  // Get first day of the next month
+  let nextMonthFirstDay = new Date(yearInt, monthInt + 1, 1).getDay();
+  // Get the last date of the previous month
+  let prevMonthLastDate = new Date(yearInt, monthInt, 0).getDate();
+
+  const daysUntilSunday = 1 + ((0 + (7 - nextMonthFirstDay)) % 7);
+  const daysSinceMonday = Math.abs(
+    date.getDate() - ((date.getDay() + 6) % 7) - 1
+  );
+
+  // Add the last dates from the previous month
+  for (let i = daysSinceMonday; i > 0; i--) {
+    dates = [
+      ...dates,
+      {
+        num: prevMonthLastDate - i + 1,
+        text: "",
+        highlighted: false,
+        day: DAYS[
+          new Date(yearInt, monthInt - 1, prevMonthLastDate - i).getDay()
+        ],
+        month: monthInt - 1,
+        currentMonth: false,
+      },
+    ];
+  }
+  // add the dates of the current month
+  for (let i = 1; i <= lastDate; i++) {
+    dates = [
+      ...dates,
+      {
+        num: i,
+        text: "",
+        highlighted: false,
+        day: DAYS[new Date(yearInt, monthInt, i - 1).getDay()],
+        month: monthInt,
+        currentMonth: true,
+      },
+    ];
+  }
+
+  // Add the first dates of the next month
+  for (let i = 1; i <= daysUntilSunday; i++) {
+    dates = [
+      ...dates,
+      {
+        num: i,
+        text: "",
+        highlighted: false,
+        day: DAYS[new Date(yearInt, monthInt + 1, i - 1).getDay()],
+        month: monthInt + 1,
+        currentMonth: false,
+      },
+    ];
+  }
+  return dates;
+};
+
+export default CalendarMonth;
