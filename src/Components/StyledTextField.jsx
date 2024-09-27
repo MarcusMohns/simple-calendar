19f9ce4bbf65d9ba7daccc384678908c5d15649e
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2024-09-27 17:56:54 +0200

    styles location state

--- src/Components/StyledTextField.jsx
+++ src/Components/StyledTextField.jsx
@@ -2,52 +2,56 @@ import React from "react";
 import TextField from "@mui/material/TextField";
 import { FormControl } from "@mui/material";
 import { Box } from "@mui/material";
-
-// Icons
+import Zoom from "@mui/material/Zoom";
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
 import SendIcon from "@mui/icons-material/Send";
-import Zoom from "@mui/material/Zoom";
+import PlaceIcon from "@mui/icons-material/Place";
+import EventIcon from "@mui/icons-material/Event";
 
-const StyledTextField = ({ handleSubmit, setText, text, label }) => {
-  const SendTooltipIcon = () => (
-    <Tooltip title="Send" placement="right" TransitionComponent={Zoom}>
-      <IconButton
-        sx={{ color: "white", cursor: "pointer" }}
-        onClick={handleSubmit}
-      >
-        <SendIcon />
-      </IconButton>
-    </Tooltip>
-  );
+const StyledTextField = ({ setText, text, label, icon }) => {
+  const SendTooltipIcon = () =>
+    icon === "location" ? (
+      <Tooltip title="Location" placement="right" TransitionComponent={Zoom}>
+        <IconButton sx={{ color: "white", cursor: "pointer" }}>
+          <PlaceIcon />
+        </IconButton>
+      </Tooltip>
+    ) : (
+      <>
+        <Tooltip title="Event" placement="right" TransitionComponent={Zoom}>
+          <IconButton sx={{ color: "white", cursor: "pointer" }}>
+            <EventIcon />
+          </IconButton>
+        </Tooltip>
+      </>
+    );
   return (
     <Box
-      component="form"
-      onSubmit={handleSubmit}
       sx={{
         display: "flex",
-        flexDirection: "column",
+        flexDirection: "row",
         alignItems: "center",
         justifyContent: "center",
         color: "white",
+        width: "100%",
       }}
     >
-      <FormControl>
+      <FormControl sx={{ width: "100%" }}>
         <TextField
           id="outlined-basic"
           label={label}
-          variant="filled"
+          variant="outlined"
           autoComplete="off"
           onChange={(e) => setText(e.target.value)}
           value={text}
-          margin="normal"
-          fullWidth
           color="white"
           sx={{
-            margin: "0px",
             input: { color: "white" },
             label: { color: "#999" },
             button: { paddingRight: "0px" },
+            width: "100%",
+            marginLeft: "5px",
           }}
           slotProps={{
             input: {
--- src/TextSection.jsx
+++ src/TextSection.jsx
@@ -1,39 +1,55 @@
 import React from "react";
+import { useState } from "react";
 import { Box } from "@mui/material";
+import { Stack } from "@mui/material";
 import StyledTextField from "./Components/StyledTextField";
 import dayjs, { Dayjs } from "dayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { TimePicker } from "@mui/x-date-pickers";
 import { renderTimeViewClock } from "@mui/x-date-pickers/timeViewRenderers";
+import Button from "@mui/material/Button";
 
 const TextSection = ({ selectedDay, saveText }) => {
   const [text, setText] = useState("");
-  const [time, setTime] = useState(dayjs("2022-04-17T15:30"));
+  const [time, setTime] = useState(dayjs("2022-04-17T12:00"));
+  const [location, setLocation] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     saveText(text);
     setText("");
+    setLocation("");
   };
 
   return (
-    <>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        width: "52%",
+        m: 2,
+        p: 2,
+      }}
+    >
+      <Box>
+        DISPLAY! {text} - {time.hour()} : {time.minute()}
+      </Box>
       <StyledTextField
         handleSubmit={handleSubmit}
         setText={setText}
         text={text}
+        icon=""
         label={"Add an event or reminder"}
       />
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "center",
-          marginTop: "20px",
-        }}
-      >
+      <Stack spacing={5} direction="row" sx={{ width: "100%", my: 2 }}>
+        <StyledTextField
+          handleSubmit={handleSubmit}
+          setText={setLocation}
+          text={location}
+          icon="location"
+          label={"Add a location"}
+        />
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <TimePicker
             sx={{
@@ -49,14 +65,11 @@ const TextSection = ({ selectedDay, saveText }) => {
             }}
           />
         </LocalizationProvider>
-        <StyledTextField
-          handleSubmit={handleSubmit}
-          setText={setText}
-          text={text}
-          label={"Add a location"}
-        />
-      </Box>
-    </>
+      </Stack>
+      <Button variant="contained" sx={{ width: "25%" }} onClick={handleSubmit}>
+        add
+      </Button>
+    </Box>
   );
 };
 
