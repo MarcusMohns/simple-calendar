3800ee91e71c1ef3fea2266ecaa451f0161fa1c2
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2024-10-02 14:14:11 +0200

    add badge

--- src/Day.jsx
+++ src/Day.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import Button from "@mui/material/Button";
+import Badge from "@mui/material/Badge";
 
 const Day = ({ day, handleHighlighted, isToday }) => {
   return (
-    <Button
-      variant="contained"
-      size="large"
-      outline="red"
-      onClick={() => handleHighlighted(day)}
-      sx={{
-        background: day.highlighted ? "#505153" : "inherit",
-        backgroundColor: isToday ? "#212121" : null,
-        opacity: day.currentMonth ? 1 : 0.5,
-        width: "100%",
-      }}
+    <Badge
+      color="primary"
+      badgeContent={day.appointments.length}
+      overlap="circular"
     >
-      {day.num}
-    </Button>
+      <Button
+        variant="contained"
+        size="large"
+        outline="red"
+        onClick={() => handleHighlighted(day)}
+        sx={{
+          background: day.highlighted ? "#505153" : "inherit",
+          backgroundColor: isToday ? "#212121" : null,
+          opacity: day.currentMonth ? 1 : 0.5,
+          width: "100%",
+        }}
+      >
+        {day.num}
+      </Button>
+    </Badge>
   );
 };
 434446;
