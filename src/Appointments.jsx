0b334518ffffd973589c9d184100ee624d8db453
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2024-10-14 10:48:07 +0200

    animate appointments

--- src/Appointments.jsx
+++ src/Appointments.jsx
@@ -1,32 +1,34 @@
 import React from "react";
 import Appointment from "./Appointment";
 import List from "@mui/material/List";
+import Collapse from "@mui/material/Collapse";
+import { TransitionGroup } from "react-transition-group";
 
 const Appointments = ({ appointments, deleteAppointment }) => {
   return (
     <List
       sx={{
         width: { xs: "100%", lg: "50%" },
+        minHeight: "300px",
         padding: 1,
         margin: 2,
       }}
     >
-      {appointments.length ? (
-        appointments.map((appointment) => (
-          <Appointment
-            text={appointment.text}
-            textImage={appointment.textImage}
-            fromTime={appointment.fromTime}
-            toTime={appointment.toTime}
-            location={appointment.location}
-            deleteAppointment={deleteAppointment}
-            id={appointment.id}
-            key={appointment.id}
-          />
-        ))
-      ) : (
-        <div>No events or appointments</div>
-      )}
+      <TransitionGroup>
+        {appointments.map((appointment) => (
+          <Collapse key={appointment.id}>
+            <Appointment
+              text={appointment.text}
+              textImage={appointment.textImage}
+              fromTime={appointment.fromTime}
+              toTime={appointment.toTime}
+              location={appointment.location}
+              deleteAppointment={deleteAppointment}
+              id={appointment.id}
+            />
+          </Collapse>
+        ))}
+      </TransitionGroup>
     </List>
   );
 };
